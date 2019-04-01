e3308eeb81c624b16722912cb425f0c5afe8e644
Drew Franklin <dmf2990@gmail.com>
2019-04-01 10:42:55 -0400

    initial

---
+++ pickup-backend/src/main/java/io/pickupwcci/pickup/PickupApplication.java
@@ -0,0 +1,13 @@
+package io.pickupwcci.pickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickupApplication.class, args);
+	}
+
+}
