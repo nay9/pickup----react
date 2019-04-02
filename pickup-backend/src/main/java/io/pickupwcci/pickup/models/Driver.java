38ca59ffc54e06a622d3c8625358be43326ffa9b
Rene G <renenoe94@hotmail.com>
2019-04-02 11:01:47 -0400

    fleshed out PickupRequestController, fleshed out Driver class, and Driver repo

--- pickup-backend/src/main/java/io/pickupwcci/pickup/models/Driver.java
+++ pickup-backend/src/main/java/io/pickupwcci/pickup/models/Driver.java
@@ -1,5 +1,45 @@
 package io.pickupwcci.pickup.models;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Driver {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String driverName;
+	@OneToMany(mappedBy = "driver")
+	private Collection<PickupRequest> pickupRequests;
+	
+	public Driver() {}
+
+	public Driver(String driverName, Collection<PickupRequest> pickupRequests) {
+		this.driverName = driverName;
+		this.pickupRequests = pickupRequests;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public Collection<PickupRequest> getPickupRequests() {
+		return pickupRequests;
+	}
+
+	@Override
+	public String toString() {
+		return "Driver [id=" + id + ", driverName=" + driverName + ", pickupRequests=" + pickupRequests + "]";
+	}
+	
+	
 }
