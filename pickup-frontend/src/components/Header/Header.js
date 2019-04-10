834873ac6f00dd22428a34f58b82a0b035801022
Drew Franklin <dmf2990@gmail.com>
2019-04-10 15:33:33 -0400

    alternate

--- pickup-frontend/src/components/Header/Header.js
+++ pickup-frontend/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './header.css'
-import Logo from '../../images/pickup.png';
+// import Logo from '../../images/pickup.png';
+import Logo from '../../images/altLogo.png';
 
 import Nav from '../Nav/Nav'
 
 export default ({ updateCurrentLocation }) => (
     <header className="site-header">
       <div className="branding">
-      <div className="logo"><img src={Logo}></img></div>
+      <div><img className="logo" src={Logo}></img></div>
       </div>
         <Nav updateCurrentLocation={updateCurrentLocation} />
     </header>
