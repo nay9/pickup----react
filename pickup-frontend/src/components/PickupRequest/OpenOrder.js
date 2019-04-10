98d68452f96abdeb5fd3e333f2f25140cb8a5a61
Drew Franklin <dmf2990@gmail.com>
2019-04-10 15:08:51 -0400

    map on all orders

--- pickup-frontend/src/components/PickupRequest/OpenOrder.js
+++ pickup-frontend/src/components/PickupRequest/OpenOrder.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './PickupRequest.css'
+import MapContainer from '../MapContainer'
 
 class OpenOrder extends Component {
 
@@ -14,15 +15,29 @@ class OpenOrder extends Component {
     return (
     <div className="box">
       <form className="" onSubmit={this.onSubmit}>
-          <h2 className="box-item">{ description }</h2>
-          <p className="box-item">Starting Location: { locationStart }</p>
-          <p className="box-item">Destination: { locationEnd }</p>
-          <p className="box-item">Time of Delivery: { time }</p>
-          <p className="box-item">Status: { status }</p>
-          <p className="">Order Confirmation: { id }</p>
-          <button className="btn" onClick={()=>this.props.assignOrder(id) }>Accept Order</button>
+        <section className="grid__section">
+          <div className="grid__item">
+            <h2 className="box-item">{ description }</h2>
+            <p className="box-item">Starting Location: { locationStart }</p>
+            <p className="box-item">Destination: { locationEnd }</p>
+            <p className="box-item">Time of Delivery: { time }</p>
+            <p className="box-item">Status: { status }</p>
+            <p className="">Order Confirmation: { id }</p>
+          </div>
+          <div className="grid__item">
+            <h1>Mapping</h1>
+              <div id="map">
+                <MapContainer />
+              </div>
+          </div>
+        </section>
+      <div id="btnM">
+        <button id="btn" onClick={()=>this.props.assignOrder(id) }>Accept Order</button>
+      </div>
       </form>
-    </div> )} 
+    </div>
+    
+    )} 
 
     else return ("")
   }
