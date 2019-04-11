c30c875403bed8297fa4d7e6ab1fab06977f47f3
Lindsey Edwards <ledwards614@gmail.com>
2019-04-11 15:07:39 -0400

    added item pic, and direction markers

--- pickup-frontend/src/components/MapContainer.js
+++ pickup-frontend/src/components/MapContainer.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
   const Map = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC6wQaQsgfm_w4qxJo2zsmyMAG_Tf5KeA8&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `195px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -17,6 +17,8 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
     defaultCenter={{ lat: 39.9611755, lng: -82.9987942 }}
   >
     {props.isMarkerShown && <Marker position={{ lat: 39.9611755, lng: -82.9987942 }} onClick={props.onMarkerClick} />}
+    {props.isMarkerShown && <Marker position={{ lat: 38.9611755, lng: -82.9987942 }} onClick={props.onMarkerClick} />}
+    {props.isMarkerShown && <Marker position={{ lat: 39.9611755, lng: -81.9987942 }} onClick={props.onMarkerClick} />}
   </GoogleMap>
 )
 
