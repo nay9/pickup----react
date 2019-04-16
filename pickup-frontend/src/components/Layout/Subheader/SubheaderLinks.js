446f9b5879420f160cb8a21211fbdf912231722d
Drew Franklin <dmf2990@gmail.com>
2019-04-16 13:26:13 -0400

    fixed

--- pickup-frontend/src/components/Layout/Subheader/SubheaderLinks.js
+++ pickup-frontend/src/components/Layout/Subheader/SubheaderLinks.js
@@ -4,7 +4,7 @@ import './subheader.css'
 
 export default ({ updateCurrentLocation }) => (
     <div className="subheader__links">
-        <button className="subheader__links_btn" onClick={() => updateCurrentLocation('user')}><i class="fas fa-truck-pickup"></i> User</button>
-        <button className="subheader__links_btn" onClick={() => updateCurrentLocation('driver')}><i class="fas fa-users"></i> Driver</button>
+        <button className="subheader__links_btn" onClick={() => updateCurrentLocation('user')}><i className="fas fa-truck-pickup"></i> User</button>
+        <button className="subheader__links_btn" onClick={() => updateCurrentLocation('driver')}><i className="fas fa-users"></i> Driver</button>
     </div>
 )  
