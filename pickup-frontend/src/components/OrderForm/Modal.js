c23699c94b3e5fbd44b0c342de810014e8e31e2f
Rene G <renenoe94@hotmail.com>
2019-04-18 14:52:41 -0400

    did some styling on modal

--- pickup-frontend/src/components/OrderForm/Modal.js
+++ pickup-frontend/src/components/OrderForm/Modal.js
@@ -11,11 +11,11 @@ const modal = (props) => {
                     opacity: props.show ? '1' : '0'
                 }}>
                 <div className="modal-header">
-                    <h1>Your Order Has Been Submitted</h1>
+                    <h1 className="modal_text">Your Order Has Been Submitted</h1>
                 </div>
                
                 <div className="modal-footer">
-                    <button className="btn-cancel" onClick={props.close}>CLOSE</button>
+                    <button className="btn-cancel" onClick={props.close}> <h2>CLOSE</h2></button>
                 </div>
             </div>
         </div>
