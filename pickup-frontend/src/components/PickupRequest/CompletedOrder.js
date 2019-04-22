d5dd43d32ff862400e3ce8e0d996167b9af7f143
Eric Hood <hoode1212@gmail.com>
2019-04-22 11:16:29 -0400

    added drivers

--- pickup-frontend/src/components/PickupRequest/CompletedOrder.js
+++ pickup-frontend/src/components/PickupRequest/CompletedOrder.js
@@ -15,7 +15,7 @@ class CompletedOrder extends Component {
         <section className="grid__section">
           <div className="grid__item gridItemA">
                 <h2 className="box-item">{ description }</h2>
-                <p className="box-item">Driver</p>
+                <p className="box-item">Driver: Alex</p>
                 <p className="box-item">Starting Location: { locationStart }</p>
                 <p className="box-item">Destination: { locationEnd }</p>
                 <p className="box-item">Date of Delivery: { date }</p>
